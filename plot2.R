4877782c6478adea783d154a03efaedb85037ca3
Iskar Waluyo <iskarwaluyo@gmail.com>
2015-02-07 14:34:54 -0600

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,3 +1,5 @@
+plot2(rows){
+
 ## READS AND SUBSETS DATA
 
 powerconsumption <- read.table("household_power_consumption.txt") ## READ TEXT FILE
@@ -10,9 +12,10 @@ col.names <- datamatrix[1] ## ASSIGNS COLUMN HEADERS
 row.names(datamatrix) <- 1:rows ## ASSIGNS NUMERIC ROWNAMES
 datamatrix <- subset(datamatrix, datamatrix[,1] == "2/2/2007" | datamatrix[,1] == "1/2/2007") ## SUBSETS TO FEB 1 AND 2
 
-## PLOT 2
-par(mfrow=c(1,1))
-plot2 <- plot(as.numeric(datamatrix2[,3]), type = "l", ylab = "Global Active Power (kilowatts)", xlab = "")
-
-
+## PLOT 2 MAKES PLOT OF THE DATA
+  par(mfrow = c(1,1))
+  plot(as.numeric(datamatrix2[,3]), type = "l", ylab = "Global Active Power (kilowatts)", xlab = "", xaxt='n')
+  axis(1, at = seq(0, 2880, by = 1440), label = c("Thu", "Fri", "Sat"), tick = T)
+  
+}
 
