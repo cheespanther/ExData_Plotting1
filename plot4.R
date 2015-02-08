62fd519d79b6f675cebab9036071eb3b6589d9b8
Iskar Waluyo <iskarwaluyo@gmail.com>
2015-02-07 19:59:14 -0600

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,4 +1,5 @@
-plot4 <- function(){
+plot4 <- function()
+{
 
 ## READS AND SUBSETS DATA
 
@@ -18,23 +19,26 @@ datamatrix <- subset(datamatrix, datamatrix[,1] == "2/2/2007" | datamatrix[,1] =
   par(mfrow = c(2,2))
   
   ##1
-  plot(as.numeric(datamatrix2[,3]), type = "l", ylab = "Global Active Power", xlab = "")  
+  plot(as.numeric(datamatrix2[,3]), type = "l", ylab = "Global Active Power", xlab = "", xaxt='n')  
   axis(1, at = seq(0, 2880, by = 1440), label = c("Thu", "Fri", "Sat"), tick = T)
   
   ##2
-  plot(as.numeric(datamatrix2[,5]), type = "l", ylab = "Voltage", xlab = "datetime")
+  plot(as.numeric(datamatrix2[,5]), type = "l", ylab = "Voltage", xlab = "datetime", xaxt='n')
   axis(1, at = seq(0, 2880, by = 1440), label = c("Thu", "Fri", "Sat"), tick = T)
   ##3
   plot(as.numeric(datamatrix2[,7]), type = "l", ylab = "Energy sub metering", col = "black", xlab = "", xaxt='n')
   points(as.numeric(datamatrix2[,8]), type = "l", ylab = "Energy sub metering", col = "orange", xlab = "", xaxt='n')
   points(as.numeric(datamatrix2[,9]), type = "l", ylab = "Energy sub metering", col = "blue", xlab = "", xaxt='n')
   legend.txt <- c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
-  legend.color <- c("black","orange","blue" )
+  legend.color <- c("black","orange","blue")
   legend("topright", legend.txt, lty = c(1,1), col = legend.color, cex = 0.25) 
   axis(1, at = seq(0, 2880, by = 1440), label = c("Thu", "Fri", "Sat"), tick = T)
   
   ##4
   plot(as.numeric(datamatrix2[,4]), type = "l", ylab = "Global_Reactive_Power", xlab = "datetime", xaxt='n')
   axis(1, at = seq(0, 2880, by = 1440), label = c("Thu", "Fri", "Sat"), tick = T)
+  
+  png("plot4.png", width = 480, height = 480)  
+  dev.off()
   
 }
