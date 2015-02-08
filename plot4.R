15dbb13b8499ea22a500bba608d8cd4a49ad18e7
Iskar Waluyo <iskarwaluyo@gmail.com>
2015-02-07 19:59:59 -0600

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -3,15 +3,15 @@ plot4 <- function()
 
 ## READS AND SUBSETS DATA
 
-powerconsumption <- read.table("household_power_consumption.txt") ## READ TEXT FILE
-rows <- nrow(as.data.frame(powerconsumption)) ## COUNT NUMBER OF ROWS
-powerconsumptionchar <- lapply(powerconsumption, as.character) ## TRANSFORM DATA TO A CHARACTER LIST
-datachar <- unlist(powerconsumptionchar) ## TRANSFROM LIST TO CHARACTER TO APPLY STRSPLIT
-data <- strsplit(datachar, split = ";") ## SPLITS ROW INTO COLUMNS
-datamatrix <- do.call(rbind,(lapply(data, rbind))) ## CREATES MATRIX WITH SPLITDATA
-col.names <- datamatrix[1] ## ASSIGNS COLUMN HEADERS
-row.names(datamatrix) <- 1:rows ## ASSIGNS NUMERIC ROWNAMES
-datamatrix <- subset(datamatrix, datamatrix[,1] == "2/2/2007" | datamatrix[,1] == "1/2/2007") ## SUBSETS TO FEB 1 AND 2
+  powerconsumption <- read.table("household_power_consumption.txt") ## READ TEXT FILE
+  rows <- nrow(as.data.frame(powerconsumption)) ## COUNT NUMBER OF ROWS
+  powerconsumptionchar <- lapply(powerconsumption, as.character) ## TRANSFORM DATA TO A CHARACTER LIST
+  datachar <- unlist(powerconsumptionchar) ## TRANSFROM LIST TO CHARACTER TO APPLY STRSPLIT
+  data <- strsplit(datachar, split = ";") ## SPLITS ROW INTO COLUMNS
+  datamatrix <- do.call(rbind,(lapply(data, rbind))) ## CREATES MATRIX WITH SPLITDATA
+  col.names <- datamatrix[1] ## ASSIGNS COLUMN HEADERS
+  row.names(datamatrix) <- 1:rows ## ASSIGNS NUMERIC ROWNAMES
+  datamatrix <- subset(datamatrix, datamatrix[,1] == "2/2/2007" | datamatrix[,1] == "1/2/2007") ## SUBSETS TO FEB 1 AND 2
 
 
 ##PLOT 4
